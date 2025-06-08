4cd43aed8f5e22bb61e1461cbc04b72a81b4d8a3
Nguyễn Lưu Phương Uyên <luuphuonguyen04@gmail.com>
2025-06-09 00:59:35 +0700

    update

--- backend/core/models.py
+++ backend/core/models.py
@@ -19,7 +19,7 @@ from django.utils.html import strip_tags
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(reset_password_token, *args, **kwargs):
-    sitelink = "http://localhost:5173/"
+    sitelink = "https://trodanang.netlify.app"
     token = "{}".format(reset_password_token.key)
     full_link = str(sitelink)+str("password-reset/")+str(token)
 
